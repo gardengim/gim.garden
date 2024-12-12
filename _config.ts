059a8d0d8955c7292245f79728cb737dae0fe051
Garden Gim <50333551+gardengim@users.noreply.github.com>
2024-12-12 12:30:35 +0900

    add basePath, jsx plugins

--- _config.ts
+++ _config.ts
@@ -1,7 +1,15 @@
 import lume from "lume/mod.ts";
 
+import basePath from "lume/plugins/base_path.ts";
+import jsx from "lume/plugins/jsx_preact.ts";
+
 const site = lume({
 	location: new URL("https://gim.garden"),
 });
+
+site
+  .use(basePath())
+  .use(jsx())
+  ;
 
 export default site;
